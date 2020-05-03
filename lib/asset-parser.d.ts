5ddeec07084473b3e4698e798f31aa4148203a78
Greg Lara <rglara@users.noreply.github.com>
2020-05-03 18:01:16 -0400

    re-add hand-written declaration file
    re-enable asset parsing
    
    fix asset URL parameters

---
+++ lib/asset-parser.d.ts
@@ -0,0 +1,22 @@
+export interface Sprite
+{
+	spriteName: string,
+	spriteBitmap: {
+		data: Buffer,
+		width: number,
+		height: number
+	}
+}
+
+export interface ParseResults
+{
+	imageName: string,
+	imageBitmap: {
+		data: Buffer,
+		width: number,
+		height: number
+	},
+	sprites: Array<Sprite>
+}
+
+export function parseAssetBundle(data: Uint8Array): ParseResults | undefined;
